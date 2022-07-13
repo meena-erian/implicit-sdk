687d94d0216d8c094eaf521dd713f8e1b1eb62b1
Menas Erian <meena5.erian@gmail.com>
2022-07-13 13:09:50 +0400

    use Type Hints

--- src/python/lib/implicit_sdk/implicit_endpoint.py
+++ src/python/lib/implicit_sdk/implicit_endpoint.py
@@ -2,8 +2,35 @@ from docstring_parser import parse
 from inspect import getfullargspec
 from flask import request, make_response
 from django.http import HttpResponse
+from typing import Optional, TypedDict
 import os
 import json
+
+
+class FunctionIdentifier(TypedDict):
+    name: Optional[str]
+    type_name: Optional[str]
+    summary: Optional[str]
+    default: Optional[str]
+
+
+class FunctionDocumentation(TypedDict):
+    summary: Optional[str]
+    params: Optional[list[FunctionIdentifier]]
+    returns: Optional[FunctionIdentifier]
+
+
+class FunctionStructure(TypedDict):
+    name: str
+    params: list[str]
+    DocComment: Optional[FunctionDocumentation]
+
+
+class EndpointFunction(TypedDict):
+    name: str
+    params: list[str]
+    DocComment: Optional[FunctionDocumentation]
+    ref: function
 
 
 class ImplicitEndpoint:
@@ -16,31 +43,38 @@ class ImplicitEndpoint:
         "array":      "object",
         "object":      "object",
         "bool":      "boolean",
-        "mixed":       ""
+        "mixed":       "any"
     }
 
-    def __init__(self, url):
-        base_attrs = {}
-        self._location = url
+    def __init__(self, url: str):
+        base_attrs: dict = {}
+        self._location: str = url
         for attr in dir(ImplicitEndpoint):
             base_attrs[attr] = type(getattr(ImplicitEndpoint, attr))
-        obj_attrs = dir(self)
-        self._functions = {}
+        obj_attrs: list[str] = dir(self)
+        self._functions: dict[str, EndpointFunction] = {}
         for attr in obj_attrs:
             obj = getattr(self, attr)
             if type(obj).__name__ == 'method' and attr not in base_attrs:
-                self._functions[attr] = self.analize_function(obj)
+                self._functions[attr] = self.__analize_function(obj)
                 self._functions[attr]['ref'] = obj
 
     @classmethod
-    def __Python_Type_To_JSType(cls, pythonType):
-        if not pythonType in cls.JSType:
-            return ""
+    def __Python_Type_To_JSType(cls, pythonType: str) -> str:
+        pythonType = pythonType.lower()
+        if '|' in pythonType:
+            type_list = pythonType.split('|')
         else:
-            return f"{{{cls.JSType[pythonType]}}}"
+            type_list = [pythonType]
+        for i in range(type_list):
+            if not type_list[i] in cls.JSType:
+                type_list[i] = "UnknownType"
+            else:
+                type_list[i] = f"{{{cls.JSType[type_list[i]]}}}"
+        return ' | '.join(type_list)
 
     @classmethod
-    def __JSDoc(cls, docArray):
+    def __JSDoc(cls, func_doc: FunctionDocumentation) -> str:
         ret = "/**\n"
 
         def commentBlock(blockStr):
@@ -50,35 +84,33 @@ class ImplicitEndpoint:
                     blockStr.split("\n")
                 )
             )
-        ret += f"{commentBlock(docArray['summary'])}\n"
-        for param in docArray["params"]:
+        ret += f"{commentBlock(func_doc['summary'])}\n"
+        for param in func_doc["params"]:
             paramStr = "@param " + \
-                cls.__Python_Type_To_JSType(param["type"].lower()) +\
+                cls.__Python_Type_To_JSType(param["type_name"]) +\
                 " " + param["name"] + " " + param["summary"]
             ret += commentBlock(paramStr) + "\n"
-        if "return" in docArray \
-                and type(docArray['return']).__name__ == 'dict':
+        if "returns" in func_doc \
+                and type(func_doc['returns']).__name__ == 'dict':
             returnStr = "@return " + \
                 cls.__Python_Type_To_JSType(
-                    docArray["return"]["type"].lower()
+                    func_doc["returns"]["type_name"]
                 ) + \
-                " " + docArray["return"]["summary"]
+                " " + func_doc["returns"]["summary"]
             ret += commentBlock(returnStr) + "\n"
         ret += " */"
         return ret
 
     @classmethod
-    def __HTMLDoc(cls, docArray):
-        if not docArray:
-            raise Exception("Warning: docArray is a None object")
+    def __HTMLDoc(cls, func_doc: FunctionDocumentation) -> str:
         ret = "<p class='indented'>" + \
-            docArray["summary"].replace("\n", "<br />") + "</p>"
+            func_doc["summary"].replace("\n", "<br />") + "</p>"
         ret += "<h4>Parameters: </h4>"
-        if len(docArray["params"]):
+        if len(func_doc["params"]):
             ret += "<ul>"
-            for param in docArray["params"]:
+            for param in func_doc["params"]:
                 paramStr = "<li><b><i>" + \
-                    param["type"].lower() + \
+                    param["type_name"] + \
                     "</i> " + param["name"] + "</b><p class='indented'>" + \
                     param["summary"] + "</p></li>"
                 ret += paramStr.replace("\n", "<br />")
@@ -87,16 +119,16 @@ class ImplicitEndpoint:
             ret += "<p>No parameters</p>"
         ret += "<h4>Return Value: </h4>"
 
-        if "return" in docArray \
-                and type(docArray['return']).__name__ == 'dict':
+        if "returns" in func_doc \
+                and type(func_doc["returns"]).__name__ == 'dict':
             retStr = "<ul><li>"
-            if "type" in docArray["return"]:
+            if "type" in func_doc["return"]:
                 retStr += "<b><i>" + \
-                    docArray["return"]["type"].lower() + \
+                    func_doc["return"]["type"].lower() + \
                     "</i></b>"
-            if "summary" in docArray["return"]:
+            if "summary" in func_doc["return"]:
                 "<p>" + \
-                    docArray["return"]["summary"] + \
+                    func_doc["return"]["summary"] + \
                     "</p>"
             retStr += "</li></ul>"
             ret += retStr.replace("\n", "<br />")
@@ -105,14 +137,14 @@ class ImplicitEndpoint:
         return ret
 
     @classmethod
-    def __parse_DocComment(cls, comment):
+    def __parse_DocComment(cls, comment: str) -> FunctionDocumentation:
         doc = parse(comment)
         summary = doc.short_description
         if hasattr(doc, 'long_description') and doc.long_description is not None:
             summary += "\n\n" + doc.long_description
         params = list(map(
             lambda param: {
-                "type": param.type_name,
+                "type_name": param.type_name,
                 "name": param.arg_name,
                 "summary": param.description
             },
@@ -121,21 +153,21 @@ class ImplicitEndpoint:
         returns = None
         if doc.returns:
             returns = {
-                "type": doc.returns.type_name,
+                "type_name": doc.returns.type_name,
                 "name": doc.returns.return_name,
                 "summary": doc.returns.description
             }
         return {
             "summary": summary,
             "params": params,
-            "return": returns
+            "returns": returns
         }
 
     @classmethod
-    def analize_function(cls, func):
-        doc_comment = func.__doc__
-        name = func.__name__
-        params = getfullargspec(func).args
+    def __analize_function(cls, func: function) -> FunctionStructure:
+        doc_comment: str = func.__doc__
+        name: str = func.__name__
+        params: list[str] = getfullargspec(func).args
         params.pop(0)  # Remove 'self' from args
         return {
             "name": name,
@@ -144,34 +176,34 @@ class ImplicitEndpoint:
         }
 
     @staticmethod
-    def get_template(file_name):
+    def __get_template(file_name: str) -> str:
         dir_path = os.path.dirname(os.path.realpath(__file__))
         template_path = os.path.join(dir_path, file_name)
         return open(template_path).read()
 
     @classmethod
-    def get_html_function_template(cls):
-        return cls.get_template("function-template.html")
+    def __get_html_function_template(cls) -> str:
+        return cls.__get_template("function-template.html")
 
-    def get_js_function_template(cls):
-        return cls.get_template("function-template.js")
-
-    @classmethod
-    def get_html_doc_template(cls):
-        return cls.get_template("doc-templete.html")
+    def __get_js_function_template(cls) -> str:
+        return cls.__get_template("function-template.js")
 
     @classmethod
-    def get_module_template(cls):
-        return cls.get_template("module-header.js")
+    def __get_html_doc_template(cls) -> str:
+        return cls.__get_template("doc-templete.html")
+
+    @classmethod
+    def __get_module_template(cls) -> str:
+        return cls.__get_template("module-header.js")
 
     @staticmethod
-    def use_template(template, params):
+    def __use_template(template: str, params: dict[str, str]) -> str:
         for param in params:
             template = template.replace(param, params[param])
         return template
 
     def reflectJS(self):
-        output = self.use_template(self.get_module_template(), {
+        output = self.__use_template(self.__get_module_template(), {
             "pathToEndpoint": self._location + "?type=api"
         })
 
@@ -179,7 +211,7 @@ class ImplicitEndpoint:
             ServerFunction = self._functions[function_name]
             output += self.__JSDoc(ServerFunction["DocComment"]) + "\n"
             argList = ", ".join(ServerFunction["params"])
-            output += self.use_template(self.get_js_function_template(), {
+            output += self.__use_template(self.__get_js_function_template(), {
                 "functionName": ServerFunction["name"],
                 "argList": argList
             })
@@ -195,12 +227,12 @@ class ImplicitEndpoint:
             definition += f"<i>{', '.join(params)}</i>"
             definition += ")"
             description = self.__HTMLDoc(ServerFunction["DocComment"])
-            content += self.use_template(self.get_html_function_template(), {
+            content += self.__use_template(self.__get_html_function_template(), {
                 "[[Function-Name]]": ServerFunction["name"],
                 "[[Function-Definition]]": definition,
                 "[[Function-Description]]": description
             })
-        return self.use_template(self.get_html_doc_template(), {
+        return self.__use_template(self.__get_html_doc_template(), {
             "[[Endpoint-Name]]": self.__class__.__name__,
             "[[Functions-List]]": content,
             "[[Module-Link]]": self._location + '?type=js'
